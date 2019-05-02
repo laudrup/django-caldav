4c13968fd1f98358e5e5dff5d6aaad634f8b598e
Kasper Laudrup <laudrup@stacktrace.dk>
2019-05-02 21:12:48 +0200

    Remove unused function

--- django_caldav/__init__.py
+++ django_caldav/__init__.py
@@ -1,8 +1,1 @@
 __author__ = 'Petr Knap <knap@wpj.cz>'
-
-
-def print_xml(xml_as_string):
-    if xml_as_string:
-        from xml.dom import minidom
-        xml_as_dom = minidom.parseString(xml_as_string)
-        print xml_as_dom.toprettyxml()
