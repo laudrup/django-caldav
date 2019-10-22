88abcd3a33c9315ff3a5899c2519f4b892eadbac
Kasper Laudrup <laudrup@stacktrace.dk>
2019-10-22 20:50:12 +0200

    Fix up runtests script
    Use normal python dict syntax
    No reason to set some options (eg. host) for an in-memory database
    Add a bit more verbosity

--- runtests.py
+++ runtests.py
@@ -5,26 +5,23 @@ from django.test.runner import DiscoverRunner
 from django.conf import settings
 
 if not settings.configured:
-    settings_dict = dict(
-        INSTALLED_APPS=(
+    settings_dict = {
+        'INSTALLED_APPS': (
             'django_caldav',
             'tests',
         ),
-        DATABASES={
-            "default": {
-                "ENGINE": "django.db.backends.sqlite3",
-                "NAME": ":memory:",
-                "USER": "",
-                "PASSWORD": "",
-                "HOST": "",
-                "PORT": "",
+        'DATABASES': {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:',
             }
         },
-        MIDDLEWARE_CLASSES=()
-    )
+        'MIDDLEWARE_CLASSES': (
+        ),
+    }
 
     settings.configure(**settings_dict)
     django.setup()
 
-failures = DiscoverRunner(verbosity=1, interactive=True, failfast=False).run_tests([])
+failures = DiscoverRunner(verbosity=2, interactive=True, failfast=False).run_tests([])
 sys.exit(failures)
